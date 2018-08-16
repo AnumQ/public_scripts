fc3d9e460fc6ee996d081bd20acfdbf1a6aed5f0
Anum Qudsia <aquds@nets.eu>
2018-08-16 14:49:49 +0200

    Added swift module script

---
+++ module.sh
@@ -0,0 +1,12 @@
+if [ -d "${BUILT_PRODUCTS_DIR}/MobileFramework" ]; then
+echo "${BUILT_PRODUCTS_DIR}/MobileFramework directory already exists, so skipping the rest of the script."
+exit 0
+fi
+
+mkdir -p "${BUILT_PRODUCTS_DIR}/MobileFramework"
+cat <<EOF > "${BUILT_PRODUCTS_DIR}/MobileFramework/module.modulemap"
+module EzioMobile {
+    header "${PROJECT_DIR}/MobileFramework.framework/Headers/MobileFramework.h"
+    export *
+}
+EOF
